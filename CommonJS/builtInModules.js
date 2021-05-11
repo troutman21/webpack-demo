b01ba5dc56b32b9f2f19360d7e587f2caae08af7
Phillip Troutman <philliptroutman21@gmail.com>
2021-05-11 10:40:58 -0400

    updated demos to be used by all academic team

---
+++ CommonJS/builtInModules.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+console.log(`http: `, http);
+console.log(`path: `, path);
+console.log(`fs: `, fs);
+console.log(`os: `, os);
+
+
+// const userInfo = os.userInfo()
+// for(const property in userInfo){
+//   console.log(`${property}: ${userInfo[property]}`);
+// }
+
+
+
+
---
+++ CommonJS/common.js
@@ -0,0 +1,8 @@
+// Step 3 require greetings from greetings.js
+const myGreetings = require('./greetings.js');
+
+// Step 4: run greeting method show we have access to the variable
+myGreetings.sayHi();
+
+//Bonus: what happens when we do this?
+const name = 'Will';
