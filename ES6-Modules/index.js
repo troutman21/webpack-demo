2b9fd3f3e398e5898381369407a526e98af60d11
Phillip Troutman <philliptroutman21@gmail.com>
2021-05-11 10:40:15 -0400

    updated demos to be used by all academic team

---
+++ ES6-Modules/index.js
@@ -0,0 +1,8 @@
+import multiplyBy2 from './multiplyBy2.js';
+import addS from './addS.js';
+import whatCohortIsBest from './whatCohortIsBest.js';
+
+
+console.log(`multiplyBy2: `, multiplyBy2(1000));
+console.log(`addS: `, addS(['pizza', 'beer', 'good time']));
+console.log(`whatCohortIsBest: `, whatCohortIsBest(20));
---
+++ ES6-Modules/whatCohortIsBest.js
@@ -0,0 +1,8 @@
+const whatCohortIsBest = irrelevantNumberArgument => {
+  // do a bunch of stuff with argument
+  const favEpisodeNumberOfTheOffice = 112 // <- Scott's Tots
+  const newVariable = irrelevantNumberArgument % 73 + favEpisodeNumberOfTheOffice * Math.PI;
+  return 'This cohort is the best cohort!!!';
+}
+
+export default whatCohortIsBest;
