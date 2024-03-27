ec518b01108274938639adf9434ee91172d710e6
Phillip Troutman <philliptroutman21@gmail.com>
2024-03-26 17:16:39 -0700

    updated fizzBuzz

--- ES6-Modules/fizzBuzz.js
+++ ES6-Modules/fizzBuzz.js
@@ -1,10 +1,11 @@
 const fizzBuzz = n => {
-  const output = "";
+  let output = "";
   for(let i = 1; i <= n; i++){
     let string = "";
     if(i % 3 === 0) string += "fizz";
     if (i % 5 === 0) string += "buzz";
-    output.push(string || i);
+    output += string || i;
+    if(i !== n) output += " ";
   }
   return output;
 }
