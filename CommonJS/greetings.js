9d5dd952d493916cae92483510fa30862d6fe695
Phillip Troutman <philliptroutman21@gmail.com>
2024-06-24 20:31:34 -0700

    reset

--- CommonJS/greetings.js
+++ CommonJS/greetings.js
@@ -4,13 +4,13 @@ console.log(`module: `, module);
 
 // step2. create an object with a method on it that references a local variable
 
-// const myGreetings = {
-//   sayHi: () => {
-//     console.log(`hello, ${name}`);
-//   },
-// };
+const myGreetings = {
+  sayHi: () => {
+    console.log(`hello, ${name}`);
+  },
+};
 
-// const name = 'Phillip';
+const name = 'Phillip';
 
 // step 3. Set that object to be module.exports
 module.exports = myGreetings;
